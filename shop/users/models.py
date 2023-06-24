bc3aa717f4885bc6f6c3b1fc0a926668692e76a5
Knyazevv <dreq12345@gmail.com>
2023-06-24 17:41:07 +0300

    gfhfg

--- shop/users/models.py
+++ shop/users/models.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.contrib.auth.models import Group, Permission
+
 
 
 class CustomUser(AbstractUser):
-    image = models.ImageField(upload_to='users_image', null=True, blank=True)
+    photo_0 = models.ImageField(upload_to='users_image', null=True, blank=True)
+    # photo_0 = models.ImageField(upload_to="photos/%Y/%m/%d/")
     is_verified_email = models.BooleanField(default=False)
 
     class Meta:
