d193f19381a06e248319a80b0ff0071ab1a63136
Knyazevv <dreq12345@gmail.com>
2023-06-26 12:23:28 +0300

    aaaaaaa

--- shop/product/models.py
+++ shop/product/models.py
@@ -38,10 +38,7 @@ class Product(models.Model):
     photo_1 = models.ImageField(upload_to="photos/%Y/%m/%d/", blank=True)
     photo_2 = models.ImageField(upload_to="photos/%Y/%m/%d/", blank=True)
     photo_3 = models.ImageField(upload_to="photos/%Y/%m/%d/", blank=True)
-    # photo_4 = models.ImageField(upload_to="photos/%Y/%m/%d/", blank=True)
-    # photo_5 = models.ImageField(upload_to="photos/%Y/%m/%d/", blank=True)
-    # photo_6 = models.ImageField(upload_to="photos/%Y/%m/%d/", blank=True)
-    # photo_7 = models.ImageField(upload_to="photos/%Y/%m/%d/", blank=True)
+
 
 
 def __str__(self):
