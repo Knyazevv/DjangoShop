f07a7d481295b1339a02ef1f7fddaa798b11b1d1
Knyazevv <dreq12345@gmail.com>
2023-06-26 18:17:15 +0300

    fix orders

--- shop/product/views.py
+++ shop/product/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from . models import Product, Category
 from functools import wraps
-from users.models import CustomUser
+from users.models import CustomUser, Order_basket
 
 
 
@@ -12,16 +12,37 @@ def context_data(func):
         products = Product.objects.order_by('name')
         categories = Category.objects.order_by('name')
         user = CustomUser.objects.order_by('username')
-       
+        user_id = Order_basket.objects.order_by('id')
         context = {
             "products": products,
             'categories': categories,
             'users': user,
-            
+            'user_id':user_id,
             
         }
         return func(request, *args, context=context, **kwargs)
     return wrapper
+
+
+
+# def context_data(func):
+#     @wraps(func)
+#     def wrapper(request, *args, **kwargs):
+#         products = Product.objects.order_by('name')
+#         categories = Category.objects.order_by('name')
+#         user = CustomUser.objects.get(username=request.user.username)
+#         user_id = Order_basket.objects.filter(user=user).first()
+#         context = {
+#             "products": products,
+#             'categories': categories,
+#             'user': user,
+#             'user_id': user_id.id if user_id else None,
+#         }
+#         return func(request, *args, context=context, **kwargs)
+#     return wrapper
+
+
+
 
 
 
