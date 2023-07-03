c0229720a5df3d500101fdfe6691bd613ae08f5f
Knyazevv <dreq12345@gmail.com>
2023-07-03 19:13:14 +0300

    maps update

--- shop/product/urls.py
+++ shop/product/urls.py
@@ -6,9 +6,8 @@ app_name = 'products'
 
 urlpatterns = [ 
     path('users/', include('users.urls') , name='users'),
-    path('users/', include('users.urls'), name='users'),
-    path('baskets/add/<int:product_id>/',
-         add_to_cart, name='add_to_cart'),
+    # path('users/', include('users.urls'), name='users'),
+    path('baskets/add/<int:product_id>/', add_to_cart, name='add_to_cart'),
     path('baskets/remove/<int:basket_id>/', remove_from_card, name='remove_from_card'),
     path('baskets/increase_quantity/<int:basket_id>/', increase_quantity, name='increase_quantity'),
     path('baskets/increase_quantity_minus/<int:basket_id>/', increase_quantity_minus, name='increase_quantity_minus'),
